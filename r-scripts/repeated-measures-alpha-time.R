313fa9d10addb439bc9fde854174b14e9daa3d59
sjc6663 <82610745+sjc6663@users.noreply.github.com>
2023-04-11 10:11:26 -0400

    clean up script

--- r-scripts/repeated-measures-alpha-time.R
+++ r-scripts/repeated-measures-alpha-time.R
@@ -1,6 +1,7 @@
 # Repeated Measures - Alpha Diversity Rumen Time Analysis
 # April 11, 2023 - SAB
 
+# setup ----
 # load phyloseq objects
 load("ps-obj/phyloseq-rumen-samples-angus-counts.RData")
 load("ps-obj/phyloseq-rumen-samples-holstein-counts.RData")
@@ -12,7 +13,7 @@ library(ggplot2)
 library(tidyr)
 library(dplyr)
 
-# get Shannon diversity
+# create data frames ----
 adivA <- data.frame(
   # "Observed" = phyloseq::estimate_richness(H_fecal_counts, measures = "Observed"),
   "Shannon" = phyloseq::estimate_richness(A_rumen_counts, measures = "Shannon"),
@@ -20,27 +21,6 @@ adivA <- data.frame(
   "Hour" = phyloseq::sample_data(A_rumen_counts)$Hour,
   "SteerID" = phyloseq::sample_data(A_rumen_counts)$Steer.ID
 )
-
-sample_data(A_rumen_counts)$"Hour" <- factor(sample_data(A_rumen_counts)$"Hour", 
-                                                 levels = c("H0", "H2", "H6", "H12", "H18"))
-sample_data(A_rumen_counts)$"Treatment" <- factor(sample_data(A_rumen_counts)$"Treatment", 
-                                                  levels = c("Control", "RPC5", "RPC10", "RPC15"))
-
-B <- plot_richness(A_rumen_counts, x="Hour", measures=c("Shannon"), title = "B", color = "Treatment") + 
-  geom_boxplot() + 
-  scale_color_manual(values = c("#a76119", "#028571", "#dfc27d", "#80cdc1")) + 
-  theme_classic() 
-  theme(legend.position = "none")
-
-adivA <- rownames_to_column(adivA, var = "SampleID")
-
-# repeated measures anova
-
-# mod <- lmer(Shannon ~ Hour*Treatment + (1|SteerID), data = adivA)
-# Anova(mod, test = "F", type = "III")
-
-anovaA <- aov(Shannon ~ Treatment*Hour + Error(SteerID), data = adivA)
-summary(anovaA) #  F = 1.090, P = 0.386, Df = 12
 
 adivH <- data.frame(
   # "Observed" = phyloseq::estimate_richness(H_fecal_counts, measures = "Observed"),
@@ -50,9 +30,20 @@ adivH <- data.frame(
   "SteerID" = phyloseq::sample_data(H_rumen_counts)$Steer.ID
 )
 
+# repeated measures anova ----
+# https://m-clark.github.io/docs/mixedModels/anovamixed.html 
+
+anovaA <- aov(Shannon ~ Treatment*Hour + Error(SteerID), data = adivA)
+summary(anovaA) #  F = 1.090, P = 0.386, Df = 12
+
 anovaH <- aov(Shannon ~ Treatment*Hour + Error(SteerID), data = adivH)
 summary(anovaH) #  F = 0.969, P = 0.488, Df = 12
 
+# hour came up significant so I checked just for curiositys sake
+testH <- aov(Shannon ~ Hour, data = adivH)
+summary(testH) # p = 0.0044**
+
+## box plots -----
 sample_data(H_rumen_counts)$"Hour" <- factor(sample_data(H_rumen_counts)$"Hour", 
                                              levels = c("H0", "H2", "H6", "H12", "H18")) 
 sample_data(H_rumen_counts)$"Treatment" <- factor(sample_data(H_rumen_counts)$"Treatment", 
@@ -64,38 +55,19 @@ A <- plot_richness(H_rumen_counts, x="Hour", measures=c("Shannon"), title = "A",
   theme_classic() + 
   theme(legend.position = "none")
 
-testH <- aov(Shannon ~ Hour, data = adivH)
-summary(testH) # p = 0.0865
+
+sample_data(A_rumen_counts)$"Hour" <- factor(sample_data(A_rumen_counts)$"Hour", 
+                                             levels = c("H0", "H2", "H6", "H12", "H18"))
+sample_data(A_rumen_counts)$"Treatment" <- factor(sample_data(A_rumen_counts)$"Treatment", 
+                                                  levels = c("Control", "RPC5", "RPC10", "RPC15"))
+
+B <- plot_richness(A_rumen_counts, x="Hour", measures=c("Shannon"), title = "B", color = "Treatment") + 
+  geom_boxplot() + 
+  scale_color_manual(values = c("#a76119", "#028571", "#dfc27d", "#80cdc1")) + 
+  theme_classic() 
+  theme(legend.position = "none")
 
 A|B
 
 ggsave(filename = "plots/alpha-div-rumen-all.pdf", dpi = 600)
-
-# break up individual to test ====
-RPC5 <- subset_samples(A_rumen_counts,
-                       Treatment == "RPC5")
-
-adiv5 <- data.frame(
-  # "Observed" = phyloseq::estimate_richness(H_fecal_counts, measures = "Observed"),
-  "Shannon" = phyloseq::estimate_richness(RPC5, measures = "Shannon"),
-  "Hour" = phyloseq::sample_data(RPC5)$Hour,
-  "SteerID" = phyloseq::sample_data(RPC5)$Steer.ID
-)
-
-test5 <- aov(Shannon ~ Hour, data = adiv5)
-summary(test5) # p = 0.25
-
-
-RPC15 <- subset_samples(A_rumen_counts,
-                       Treatment == "RPC15")
-
-adiv15 <- data.frame(
-  # "Observed" = phyloseq::estimate_richness(H_fecal_counts, measures = "Observed"),
-  "Shannon" = phyloseq::estimate_richness(RPC15, measures = "Shannon"),
-  "Hour" = phyloseq::sample_data(RPC15)$Hour,
-  "SteerID" = phyloseq::sample_data(RPC15)$Steer.ID
-)
-
-test15 <- aov(Shannon ~ Hour, data = adiv15)
-summary(test15) # p = 0.374
 
